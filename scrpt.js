dca2ce4043fb282008de5e2c6c052ee3199f910f
Rapoza <141277901+bairosluizkaick@users.noreply.github.com>
2025-05-23 11:11:10 -0300

    ok

---
+++ scrpt.js
@@ -0,0 +1,17 @@
+const caixaPrincipal = document . querySelector(".caixaPricipal");
+const caixaPerguntas = document . querySelector(".caixaPerguntas");
+const caixaAlternativas = document . querySelector(".caixa-alternativas");
+const caixaResultado = document . querySelector(".caixaResultado");
+const textoResultado = document . querySelector(".textoResultado");
+
+const perguntas[
+ {
+    enunciado: "pergunta1",
+    alternativas:["alternativa1","alternativa2"]
+ },
+
+ {
+    enunciado: "pergunta2",
+    alternativas:["alternativa1","alternativa2"]
+ }
+]

da8e501bba60b0a8ace9653e9efe87293aa094f9
Rapoza <141277901+bairosluizkaick@users.noreply.github.com>
2025-05-28 10:35:20 -0300

    ok

--- scrpt.js
+++ scrpt.js
@@ -1,17 +1,46 @@
-const caixaPrincipal = document . querySelector(".caixaPricipal");
-const caixaPerguntas = document . querySelector(".caixaPerguntas");
-const caixaAlternativas = document . querySelector(".caixa-alternativas");
-const caixaResultado = document . querySelector(".caixaResultado");
-const textoResultado = document . querySelector(".textoResultado");
-
+const caixaPrincipal = document.querySelector(".caixaPricipal");
+const caixaPerguntas = document.querySelector(".caixaPerguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixaResultado");
+const textoResultado = document.querySelector(".textoResultado");
 const perguntas[
- {
-    enunciado: "pergunta1",
-    alternativas:["alternativa1","alternativa2"]
- },
-
- {
-    enunciado: "pergunta2",
-    alternativas:["alternativa1","alternativa2"]
- }
+   {
+      enunciado: "pergunta1",
+      alternativas: [
+         {
+            texto: "alternativa1",
+            afirmacao: "afirmacao1",
+         },
+         {
+            texto: "alternativa2",
+            afirmacao: "afirmacao2",
+         },
+      ]
+   },
+   {
+      enunciado: "pergunta2",
+      alternativas: [
+         {
+            texto: "alternativa1",
+            afirmacao: "afirmacao1",
+         },
+         {
+            texto: "alternativa2",
+            afirmacao: "afirmacao2",
+         },
+      ]
+   },
+   {
+      enunciado: "pergunta2",
+      alternativas: [
+         {
+            texto: "alternativa1",
+            afirmacao: "afirmacao1",
+         },
+         {
+            texto: "alternativa2",
+            afirmacao: "afirmacao2",
+         },
+      ]
+   },
 ]

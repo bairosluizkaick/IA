51747d60ce21e1b915316d691b5e022522712b07
Rapoza <141277901+bairosluizkaick@users.noreply.github.com>
2025-06-18 11:24:26 -0300

    ok

--- script.js
+++ script.js
@@ -5,29 +5,30 @@ const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
 const perguntas = [
     {
-        enunciado: "Como resolver equação do segundo grau?",
+        enunciado: "Qual das seguintes afirmações sobre a história do Corinthians está correta??",
         alternativas: [
             {
-                texto: "aplicar a formula de Bhaskara",
-                afirmacao: "Aplicando a formula de Bhaskara voce pode resolver de forma mais demorada"
+                texto: "a) O Corinthians foi fundado em 1910 por um grupo de operários na cidade de São Paulo, e seu nome foi uma homenagem ao Corinthian Football Club, um time amador inglês que excursionava pelo Brasil na época.",
+                afirmacao: "O Corinthians nasceu com a essência popular, moldando sua identidade ligada ao povo."
                
             },
             {
-                texto: "Soma e produto",
-                afirmacao: "usando soma e produto voce pode economizar tempo"
+                texto: "O Corinthians é o único clube brasileiro a conquistar o Mundial de Clubes da FIFA por duas vezes (em 2000 e 2012), sendo o primeiro a levantar a taça no formato atual da competição.",
+                afirmacao: "Essa conquista dupla solidifica o Corinthians como um gigante no cenário global do futebol."
             },
         ]
     },
     {
-        enunciado: "Pergunta2",
+        enunciado: "Qual das seguintes afirmações descreve corretamente um aspecto tático ou técnico importante no futebol moderno?",
         alternativas: [
             {
-                texto: "alternativa1",
-                afirmacao: "afirmacao1"
+                texto: "O "tiki-taka", estilo de jogo imortalizado pelo Barcelona de Pep Guardiola, prioriza a posse de bola intensa, com passes curtos e rápidos, buscando desorganizar a defesa adversária e criar espaços.",
+                afirmacao: "O tiki-taka revolucionou a forma de controlar o jogo e criar oportunidades no ataque."
             },
             {
-                texto: "alternativa2",
-                afirmacao: "afirmacao2"
+                texto: "O VAR (Árbitro Assistente de Vídeo) é uma tecnologia implementada para revisar lances capitais, como gols, pênaltis e cartões vermelhos diretos, visando diminuir erros cruciais e aumentar a justiça nas partidas.
+",
+                afirmacao: "O VAR busca tornar o jogo mais justo, corrigindo equívocos importantes que antes passavam despercebidos."
             },
         ]
     },
